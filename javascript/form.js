eefda6aee27abd07df2d7d09a6bf05e28845ff73
cathamm96 <cathamm96@gmail.com>
2021-11-18 18:30:06 -0500

    finalmaybe

--- javascript/form.js
+++ javascript/form.js
@@ -4,17 +4,11 @@ function validateForm() {
         alert ("first name must be filled out");
         return false;
     }
-}
-
-function validateForm() {
     let x = document.forms["submitform"]["lname"].value;
     if (x == "") {
         alert ("last name must be filled out");
         return false;
     }
-}
-
-function validateForm() {
     let x = document.forms["submitform"]["email"].value;
     if (x == "") {
         alert ("email must be filled out");
